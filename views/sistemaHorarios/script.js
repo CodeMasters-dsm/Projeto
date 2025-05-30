13035f5e886ca53a904d07c26ba4173bba019e02
manuelalemes <manuelalucia.mc@gmail.com>
2025-05-30 18:26:35 -0300

    - Correção de estrutura de sistemaHorarios; - Retirada do acento; - Subindo lauch.json.

---
+++ views/sistemaHorarios/script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('expandida');
+}
+
+function mostrarSecao(secao) {
+    const conteudo = document.getElementById("conteudo-dinamico");
+    const profile = document.getElementById("profile-section");
+}
