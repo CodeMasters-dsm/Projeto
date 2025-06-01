7c85dda249f9ce4c5dce259598fa20e5ceb74092
PehSeasmoke <penunesclaudino@gmail.com>
2025-06-01 19:34:23 -0300

    Update usersRoutes.js

--- CRUD/usersRoutes.js
+++ CRUD/usersRoutes.js
@@ -9,3 +9,11 @@ import {
 } from '../controllers/usersController.js'
 
 const router = express.Router()
+
+router.get('/', getUsers)
+router.get('/:id', getUsersById)
+router.post('/', createUsers)
+router.put('/:id', updateUsers)
+router.delete('/:id', deleteUsers)
+
+export default router
